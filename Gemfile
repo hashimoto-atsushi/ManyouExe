1e5f2ba927479b14ce4e12782379812daccbdc83
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-20 11:10:18 +0900

    RSpecインストール完了

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -26,8 +28,7 @@ end
 
 group :test do
   gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
-  gem 'chromedriver-helper'
+  gem 'webdrivers'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,15 @@ module ManyouExe
   class Application < Rails::Application
     config.load_defaults 5.2
     config.time_zone = "Asia/Tokyo"
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                  model_specs: true,
+                   view_specs: false,
+                 helper_specs: false,
+                routing_specs: false,
+             controller_specs: false,
+                request_specs: false
+      end
   end
 end
