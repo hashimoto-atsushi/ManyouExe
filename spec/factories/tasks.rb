57c6a2c65b5231a9064d22c7dfa3e88d3599d86c
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-20 11:23:39 +0900

    RSpecテスト記述済み テスト実行済み

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :task do
+    task_name { 'task1' }
+    task_detail{ 'task_detail1' }
+  end
+end
---
+++ spec/system/task.spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'task_task_name', with: 'TASK1'
+        fill_in 'task_task_detail',with: 'TASK1_DETAIL'
+        click_on 'Create Task'
+        expect(page).to have_content '1'
+      end
+    end
+  end
+
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        task = FactoryBot.create(:task, task_name: 'task2', task_detail: "task_detail2")
+        visit tasks_path
+        expect(page).to have_content 'task2'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
+         task = FactoryBot.create(:task, id:'5', task_name: 'task_Nini', task_detail: 'detail_Nini')
+         visit tasks_path
+         visit task_path(5)
+         expect(page).to have_content 'Nini'
+       end
+     end
+  end
+end
