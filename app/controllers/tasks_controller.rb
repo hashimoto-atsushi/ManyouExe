9a9032083c3ad5c2bd98cf6a5080734ea8814e7c
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-20 10:43:27 +0900

    タスク一覧画面、Createアクションを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,30 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task.id), notice: "新規タスクを登録しました!"
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :task_detail)
+  end
+
+  def set_tasks
+    @task = Task.find(params[:id])
+  end  
 end
