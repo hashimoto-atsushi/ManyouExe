3d0ad9841c56c58cc7a3a4d9da81a330366f85e2
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-25 18:51:46 +0900

    Bcrypt gemインストール済み Userコントローラー作成 モデル内Validation記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :tasks
+  validates :name, present: true,
+                   length: {maximum: 30}
+  validates :email, present: true,
+                     length: {maximum: 128}
+                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :passowrd, present: true,
+                        length: { minimum: 6 }
 end
--- db/migrate/20201025085446_create_users.rb
+++ db/migrate/20201025085446_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name, null: false, limit: 30
       t.string :email, null: false, limit: 128
-      t.string :password_digest, null: false, limit: 8
+      t.string :password_digest, null: false, limit: 16
       t.boolean :admin, null: false, default: false
       t.timestamps
       t.index :email, unique: true
