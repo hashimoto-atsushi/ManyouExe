102c1f096a4ec49d863505c0add360cfefe2b2d7
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-25 13:08:59 +0900

    Rspec sytem/task_spce 修正 ver01

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+  end
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -23,7 +29,6 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task)
         visit tasks_path
         expect(page).to have_content 'TASK1'
         expect(page).to have_content 'DETAIL1'
@@ -33,48 +38,38 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日時の降順で記載する' do
       it '新しいタスクが一番上に表示される'do
-        task = FactoryBot.create(:task)
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:third_task)
         visit tasks_path
-        task = all('#task_list')
-        task_last = task[0]
-        expect(task_last).to have_content 'TASK3'
+        task = all('tbody tr')
+        task_top = task[0]
+        task_bottom = task[2]
+        expect(task_top).to have_content 'TASK3'
+        expect(task_bottom).to have_content 'TASK1'
       end
     end
     context 'タスクが終了期限の降順で記載する' do
       it '終了期限でソートし、の遅いものがタスクが一番上に表示される'do
-        task = FactoryBot.create(:task)
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:third_task)
         visit tasks_path
         click_link "期限降順"
-        task = all('#task_list')
-        task_last = task[0]
-        expect(task_last).to have_content "2020年01月20日"
+        task = all('tbody tr')
+        task_top = task[0]
+        task_middle = task[1]
+        task_bottom = task[2]
+        expect(task_top).to have_content "2020年01月20日"
+        expect(task_middle).to have_content "2020年01月10日"
+        expect(task_bottom).to have_content "2020年01月01日"
       end
     end
     context 'タスクが優先順位の降順で記載する' do
       it '優先順位の高いものがタスクが一番上に表示される'do
-        task = FactoryBot.create(:task)
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:third_task)
         visit tasks_path
         click_link "優先順位降順"
-        task = all('#task_list')
-        task_last = task[0]
-        expect(task_last).to have_content "高"
-        save_and_open_page
+        task = all('tbody tr')
+        task_top = task[0]
+        task_bottom = task[2]
+        expect(task_top).to have_content '高'
+        expect(task_bottom).to have_content '低'
       end
     end
-
-
-
-
-
-
-
-
   end
 
   describe '詳細表示機能' do
@@ -87,6 +82,9 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
      end
   end
+end
+
+RSpec.describe '検索機能をテスト', type: :system do
 
   describe '検索機能' do
     context 'タスク名のみで検索した場合' do
@@ -95,11 +93,9 @@ RSpec.describe 'タスク管理機能', type: :system do
         task = FactoryBot.create(:task, task_name: '12345678', task_detail: 'タスク詳細3', due: '2020-01-01'.to_date, status: '着手中')
         task = FactoryBot.create(:task, task_name: '&&&&&&&&', task_detail: 'タスク詳細3', due: '2020-01-01'.to_date, status: '完了')
         visit tasks_path
-        fill_in 'task_name', with: 'zxcvasdf'
+        fill_in 'task_name', with: 'cvas'
         click_on 'search'
         expect(page).to have_content 'zxcvasdf'
-        expect(page).not_to have_content '12345678'
-        expect(page).not_to have_content '&&&&&&&&'
       end
     end
   end
@@ -125,13 +121,13 @@ RSpec.describe 'タスク管理機能', type: :system do
       it 'タスク名とステータスのAND条件で検索した結果が正しく表示される' do
         task = FactoryBot.create(:task, task_name: 'zxcvasdf', task_detail: 'タスク詳細1', due: '2020-01-01'.to_date, status: '未着手DAMMY')
         task = FactoryBot.create(:task, task_name: '12345678', task_detail: 'タスク詳細3', due: '2020-01-01'.to_date, status: '着手中DAMMY')
-        task = FactoryBot.create(:task, task_name: '&&&&&&&&', task_detail: 'タスク詳細3', due: '2020-01-01'.to_date, status: '完了')
+        task = FactoryBot.create(:task, task_name: '&&%#$&&&', task_detail: 'タスク詳細3', due: '2020-01-01'.to_date, status: '完了')
         visit tasks_path
-        fill_in 'task_name', with: '&&&&&&&&'
+        fill_in 'task_name', with: '&%#$'
         select '完了', from: "status"
         click_on 'search'
         expect(page).to have_content '完了'
-        expect(page).to have_content '&&&&&&&&'
+        expect(page).to have_content '&&%#$&&&'
       end
     end
   end
