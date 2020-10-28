757197446a3bf8d3e687af4cfafba17453ea941b
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-28 11:52:54 +0900

    全Rspecテスト 実装（微調整前）

--- spec/system/admin_spec.rb
+++ spec/system/admin_spec.rb
@@ -85,7 +85,6 @@ RSpec.describe '管理者の登録テスト', type: :system do
         task = all('tbody tr')[1].click_link '編集'
         page.driver.browser.switch_to.alert.text.should == '編集してもいいですか？'
         page.driver.browser.switch_to.alert.accept
-        binding.irb
         fill_in 'user_name', with: '山田太郎2'
         fill_in 'user_email',with: 'yamada2@example.com'
         fill_in 'user_password', with: 'yamadayamada'
@@ -93,6 +92,26 @@ RSpec.describe '管理者の登録テスト', type: :system do
         click_on '更新する'
         expect(page).to have_content '山田太郎2'
         expect(page).to have_content 'yamada2@example.com'
+      end
+    end
+  end
+  describe '管理者に関する動作確認' do
+    context '管理者でログインした場合' do
+      it '編集画面からユーザーの削除ができる' do
+        visit new_session_path
+        fill_in 'session_email',with: 'admin@example.com'
+        fill_in 'session_password', with: 'adminadmin'
+        click_on 'ログインする'
+        expect(page).to have_content 'タスク一覧'
+        click_link 'ユーザー管理画面へ'
+        expect(page).to have_content 'ユーザー管理画面'
+        expect(page).to have_content '山田太郎'
+        expect(page).to have_content 'yamada@example.com'
+        task = all('tbody tr')[1].click_link '削除'
+        page.driver.browser.switch_to.alert.text.should == '削除してもいいですか？'
+        page.driver.browser.switch_to.alert.accept
+        expect(page).not_to have_content '山田太郎'
+        expect(page).not_to have_content 'yamada@example.com'
       end
     end
   end
