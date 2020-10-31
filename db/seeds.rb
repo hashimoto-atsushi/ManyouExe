47f36c0f3ad717bc94aa270d79ffa4516fcf80b4
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-31 16:54:54 +0900

    STEP5 提出 ver01

--- db/seeds.rb
+++ db/seeds.rb
@@ -173,16 +173,3 @@ Task.create!(
   ]
 )
 end
-
-
-
-
-# Sticker.create!(sticker_name: sticker_name)
-# sticker_name = "社外秘"
-# Sticker.create!(sticker_name: sticker_name)
-# sticker_name = "共有"
-# Sticker.create!(sticker_name: sticker_name)
-# sticker_name = "告知"
-# Sticker.create!(sticker_name: sticker_name)
-# sticker_name = "重要"
-# Sticker.create!(sticker_name: sticker_name)
